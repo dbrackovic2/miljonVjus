406a2e4038c7803574108fc2bb931a54cce56cf9
Dajan Bračković <dajanb@comtrade.com>
2016-12-20 11:32:13 +0100

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var coolAsciFaces = require('cool-ascii-faces');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -9,11 +10,15 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
+app.get('/cool', function (request, response) {
+  response.send(coolAsciFaces());
+});
+
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
 
