9281f511c44c6f7e7785edf5d05fd1ba0d650e6a
Dajan Bračković <dajanb@comtrade.com>
2016-12-21 14:10:07 +0100

    add protractor dependencies

---
+++ milionVjus/conf.js
@@ -0,0 +1,18 @@
+exports.config = {
+    seleniumAddress : "http://localhost:4444/wd/hub",
+    multiCapabilities:[{
+        browserName: 'firefox',
+        version: '45',
+        shardTestFiles: true,
+        maxInstances: 5,
+        specs: [
+        'run.js']
+    },{
+        browserName: 'chrome',
+        shardTestFiles: true,
+        maxInstances:5,
+        specs:[
+        'run.js'
+        ]
+    }]
+};
---
+++ milionVjus/run.js
@@ -0,0 +1,8 @@
+for(i=0; i<100; i++){
+    browser.driver.manage().deleteAllCookies();
+    browser.driver.get("https://www.youtube.com/watch?v=DJGyyp2uxAk");
+    console.log('iteracija ' + i);
+    browser.driver.sleep(400000);
+}
+
+
