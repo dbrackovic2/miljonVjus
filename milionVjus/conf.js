221ca81f921db2367fc82e47efc251f35e56d67c
Dajan Bračković <dajanb@comtrade.com>
2016-12-21 15:22:59 +0100

    remove firefox

--- milionVjus/conf.js
+++ milionVjus/conf.js
@@ -2,13 +2,6 @@ exports.config = {
     directConnect: true,
     seleniumAddress : "http://localhost:4444/wd/hub",
     multiCapabilities:[{
-        browserName: 'firefox',
-        version: '45',
-        shardTestFiles: true,
-        maxInstances: 5,
-        specs: [
-        'run.js']
-    },{
         browserName: 'chrome',
         shardTestFiles: true,
         maxInstances:5,
