519ac7cd5595b6d5ecc561b9e38a19908b301fb7
Dajan Bračković <dajanb@comtrade.com>
2016-12-21 16:36:23 +0100

    run.js update

--- milionVjus/run.js
+++ milionVjus/run.js
@@ -1,6 +1,9 @@
 for(i=0; i<100; i++){
     browser.driver.manage().deleteAllCookies();
     browser.driver.get("https://www.youtube.com/watch?v=DJGyyp2uxAk");
+    browser.driver.window.open('https://www.youtube.com/watch?v=1V7pU1IPScU');
+    browser.driver.window.open('https://www.youtube.com/watch?v=N9ZFy7pCRGw');
+    browser.driver.window.open('https://www.youtube.com/watch?v=EggAULk8K-8');
     console.log('iteracija ' + i);
     browser.driver.sleep(400000);
 }
