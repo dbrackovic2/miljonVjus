8930a81b2a8ec0ac8824c20d4f0a1b4a3cf78de3
Dajan Bračković <dajanb@comtrade.com>
2016-12-22 13:09:01 +0100

    use recursion

--- milionVjus/run.js
+++ milionVjus/run.js
@@ -1,9 +1,24 @@
-for(i=0; i<100; i++){
+(function recursion(i) {
     browser.driver.manage().deleteAllCookies();
-    browser.driver.get("https://www.youtube.com/watch?v=DJGyyp2uxAk");
-    browser.driver.window.open('https://www.youtube.com/watch?v=1V7pU1IPScU');
-    browser.driver.window.open('https://www.youtube.com/watch?v=N9ZFy7pCRGw');
-    browser.driver.window.open('https://www.youtube.com/watch?v=EggAULk8K-8');
+    browser.driver.get("https://www.youtube.com/watch?v=DJGyyp2uxAk").then(function () {
+        browser.driver.manage().deleteAllCookies();
+    });
+    browser.driver.executeScript("window.open('https://www.youtube.com/watch?v=1V7pU1IPScU')").then(function () {
+        browser.driver.manage().deleteAllCookies();
+    });
+    browser.driver.executeScript("window.open('https://www.youtube.com/watch?v=N9ZFy7pCRGw')").then(function () {
+        browser.driver.manage().deleteAllCookies();
+    });
+    browser.driver.executeScript(" window.open('https://www.youtube.com/watch?v=EggAULk8K-8')").then(function () {
+        browser.driver.manage().deleteAllCookies();
+    });
+    i = i++;
     console.log('iteracija ' + i);
     browser.driver.sleep(400000);
-}
+    if (i == 50) return;
+    recursion(i);
+})(recursion(0));
+
+
+
+
