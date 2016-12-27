f0417d00e3ef7dce50dcec851a0d06b26a86cbbc
Dajan Bračković <dajanb@comtrade.com>
2016-12-27 14:52:46 +0100

    run.js

--- milionVjus/run.js
+++ milionVjus/run.js
@@ -1,6 +1,7 @@
-(function recursion(i) {
+(function recursion(i)  {
+    if(i === undefined) i = 0;
     browser.driver.manage().deleteAllCookies();
-    browser.driver.get("https://www.youtube.com/watch?v=DJGyyp2uxAk").then(function () {
+    browser.driver.get("https://www.youtube.com/watch?v=EggAULk8K-8").then(function () {
         browser.driver.manage().deleteAllCookies();
     });
     browser.driver.executeScript("window.open('https://www.youtube.com/watch?v=1V7pU1IPScU')").then(function () {
@@ -9,15 +10,25 @@
     browser.driver.executeScript("window.open('https://www.youtube.com/watch?v=N9ZFy7pCRGw')").then(function () {
         browser.driver.manage().deleteAllCookies();
     });
-    browser.driver.executeScript(" window.open('https://www.youtube.com/watch?v=EggAULk8K-8')").then(function () {
+    browser.driver.executeScript("window.open('https://www.youtube.com/watch?v=DJGyyp2uxAk')").then(function () {
         browser.driver.manage().deleteAllCookies();
     });
-    i = i++;
+    browser.driver.executeScript("window.open('https://www.youtube.com/watch?v=8V3AZoxkyiw')").then(function () {
+        browser.driver.manage().deleteAllCookies();
+    });
+    browser.driver.executeScript("window.open('https://www.youtube.com/watch?v=QkRIGcoOEzM')").then(function () {
+        browser.driver.manage().deleteAllCookies();
+    });
+    browser.driver.executeScript("window.open('https://www.youtube.com/watch?v=F3tbEhSwkJQ')").then(function () {
+        browser.driver.manage().deleteAllCookies();
+    });
+
+    https://www.youtube.com/watch?v=F3tbEhSwkJQ
     console.log('iteracija ' + i);
     browser.driver.sleep(400000);
     if (i == 50) return;
-    recursion(i);
-})(recursion(0));
+    recursion(++i);
+})();
 
 
 
